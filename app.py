975a0717a7c51b9db98607f49389c1d5f49b16ca
Marllon <marllongomess@gmail.com>
2026-02-24 09:19:58 -0300

    fix eventlet monkey patch

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, redirect, session, send_file, url_for
 from werkzeug.utils import secure_filename
 from flask_socketio import SocketIO, emit, join_room
