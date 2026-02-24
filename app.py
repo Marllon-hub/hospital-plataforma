54fb253ee69f66f835ea9fb80cfb5dc801130d31
Marllon <marllongomess@gmail.com>
2026-02-24 10:08:03 -0300

    fix indentation error in app.py

--- app.py
+++ app.py
@@ -1906,6 +1906,6 @@ def importar_funcionarios():
 # ==================================================
 # START
 # ==================================================
- if __name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
